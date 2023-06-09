fd101362dd5ff65e85aaf916d6561e041d6bae1d
Walker Le <walker.le88@gmail.com>
2023-06-09 16:49:01 -0600

    javascript complete

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,29 @@
 function findShortestString(arr) {
   // type your code here
+  // console.log('arr', arr);
+
+  const arrLength = arr.length;
+  // console.log('arrLength', arrLength);
+
+  let shortestStr = arr[0];
+  let shortestIndex = arr[0].length;
+  for(i = 0; i < arr.length; i++) {
+  //   // console.log('string', arr[i]);
+  //   // console.log('string length', arr[i].length);
+    if(arr[i].length < shortestIndex) {
+      shortestStr = arr[i];
+      shortestIndex = arr[i].length;
+    }
+  }
+
+  // console.log('Final shortestStr', shortestStr);
+  // console.log('Final shortestIndex', shortestIndex);
+
+  // ALTERNATE SOLUTION:
+  // arr.forEach(element => shortestStr = (element.length < shortestStr.length ? element : shortestStr))
+  // ---------------------------------------
+
+  return shortestStr;
 }
 
 if (require.main === module) {
@@ -24,3 +48,8 @@ module.exports = findShortestString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// Loop through each string within array
+// Set a variable for the shortest string
+// Set a variable for the index of shortest string
+// Return the shortest string
